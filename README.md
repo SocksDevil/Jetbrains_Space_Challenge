d63a2811350c1546ccde9f6c7719789185435287
Andre Rocha <andrefmrocha@live.com.pt>
2019-12-27 23:18:36 +0000

    Finished solution for said problem

---
+++ README.md
@@ -0,0 +1,10 @@
+# JetBrains Space Challenge
+Simple program with the ability to retrieve information regarding Java classes names popularity in Github.
+
+# Running
+To run this, due to the high amount of requests needed, it is necessary to add a `.env` file with your personal authentication information. To do this, you will need to generate a new *Personal Access Token* with no specified scope. In the dotenv file, add the following information:
+
+```
+USERNAME="USERNAME"
+TOKEN="TOKEN"
+``` 
--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -3,38 +3,64 @@ package com.andrefmrocha.jetbrains
 import com.andrefmrocha.jetbrains.model.FileSearch
 import com.andrefmrocha.jetbrains.model.Repository
 import com.andrefmrocha.jetbrains.model.RepositorySearch
+import com.github.kittinunf.fuel.core.extensions.authentication
 import com.github.kittinunf.fuel.httpGet
+import io.github.cdimascio.dotenv.dotenv
+import tech.tablesaw.api.DoubleColumn
+import tech.tablesaw.api.StringColumn
+import tech.tablesaw.api.Table
+import tech.tablesaw.plotly.Plot
+import tech.tablesaw.plotly.api.VerticalBarPlot
 
 const val API_URL = "https://api.github.com"
 
-fun main(args: Array<String>) {
+fun main() {
+    val dotenv = dotenv()
 
-    val repos = ArrayList<Repository>();
-    for (i in 0..1){
-        val (_, _, result) = "$API_URL/search/repositories?q=language:java&sort=stars&page=$i"
-            .httpGet()
-            .responseObject(RepositorySearch.Deserializer())
+    dotenv["USERNAME"]?.let { username ->
+        dotenv["TOKEN"]?.let { token ->
+            val repos = ArrayList<Repository>();
+            for (i in 0..10){
+                val (_, _, result) = "$API_URL/search/repositories?q=language:java&sort=stars&page=$i"
+                    .httpGet()
+                    .authentication()
+                    .basic(username, token)
+                    .responseObject(RepositorySearch.Deserializer())
 
-        val (repositorySearch, _) = result
-        val repositories = repositorySearch?.items
-        repositories?.forEach { repository ->  repos.add(repository)}
-    }
+                val (repositorySearch, _) = result
+                val repositories = repositorySearch?.items
+                repositories?.forEach { repository ->  repos.add(repository)}
+            }
 
-    val fileNames = HashMap<String, Int>()
+            val fileNames = HashMap<String, Int>()
 
-    repos.forEach { repository ->
-        val (_, _, result) = "$API_URL/search/code?q=language:java+repo:${repository.full_name}"
-            .httpGet()
-            .responseObject(FileSearch.Deserializer())
+            repos.forEach { repository ->
+                val (_, _, result) = "$API_URL/search/code?q=language:java+repo:${repository.full_name}"
+                    .httpGet()
+                    .authentication()
+                    .basic(username, token)
+                    .responseObject(FileSearch.Deserializer())
 
-        val (files, _) = result
+                val (files, _) = result
 
-        files?.items?.forEach { item ->
-            val name = item.name.removeSuffix(".java")
-            val value = fileNames.getOrDefault(name, 0);
-            fileNames[name] = value + 1
+                files?.items?.forEach { item ->
+                    val name = item.name.removeSuffix(".java")
+                    val value = fileNames.getOrDefault(name, 0)
+                    fileNames[name] = value + 1
+                }
+            }
+
+            val nameColumn = "names"
+            val valuesColumn = "values"
+            val tableName = "Popularity of Java class names"
+            val classNames: Table = Table.create(tableName)
+                .addColumns(
+                    StringColumn.create(nameColumn, fileNames.keys.toTypedArray()),
+                    DoubleColumn.create(valuesColumn, fileNames.values.toTypedArray())
+                )
+
+            Plot.show(VerticalBarPlot.create(tableName,
+                classNames.sortDescendingOn(valuesColumn).first(20), nameColumn, valuesColumn))
         }
     }
-
-    println(fileNames)
 }
